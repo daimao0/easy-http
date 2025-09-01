c3b4c2e477cd6c6071e62d83da41eb7b9de8304f
cy <daimao2817@gmail.com>
2025-09-01 18:30:42 +0800

    feat(httpUtil): 适配jackson的老版本

--- src/main/java/com/cy/easyhttp/util/EasySerializer.java
+++ src/main/java/com/cy/easyhttp/util/EasySerializer.java
@@ -1,7 +1,6 @@
 package com.cy.easyhttp.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
